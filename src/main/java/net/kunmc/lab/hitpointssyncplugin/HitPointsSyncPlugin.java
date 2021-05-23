5cc11e5be114f55c90a71bc7b9e9435f3c0770ef
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-05-24 01:39:29 +0900

    chore: Initial commit

---
+++ src/main/java/net/kunmc/lab/hitpointssyncplugin/HitPointsSyncPlugin.java
@@ -0,0 +1,11 @@
+package net.kunmc.lab.hitpointssyncplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HitPointsSyncPlugin extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    }
+}
